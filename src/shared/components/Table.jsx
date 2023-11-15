edfa4dc768072701e9ff4f202867968ad7c36eea
Kristoffer Velazquez <kristoffervelazquez49@live.com>
2023-11-15 02:44:15 -0700

    Add assignment details to table and handle click event.

--- src/shared/components/Table.jsx
+++ src/shared/components/Table.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './css/table.css';
+import Assignment from '../../classes/assignment';
 
 function Table({ rows, columns }) {
+  const handleClick = async (row) => {
+    const assignment = await Assignment.getAssignmentById(row.ID);
+    console.log(assignment);
+  }
   return (
     <div className="tareas-container">
       <table className="tareas-table">
@@ -10,17 +15,22 @@ function Table({ rows, columns }) {
             {columns.map((column, index) => (
               <th key={index}>{column}</th>
             ))}
-            <th>Subir</th>
+            <th>Acción</th>
 
           </tr>
         </thead>
         <tbody>
           {rows.map((row, rowIndex) => (
             <tr key={rowIndex}>
-              {columns.map((column, cellIndex) => (
-                <td key={cellIndex}>{row[column]}</td>
-              ))}
-              <td><button className='blue-btn'>Responder</button></td>
+              {columns.map((column, cellIndex) => {
+                if (column === 'ID') return (
+                  <td key={cellIndex}>{row[column].substring(0, 7)}</td>
+                )
+                return (
+                  <td key={cellIndex}>{row[column]}</td>
+                )
+              })}
+              <td><button onClick={() => handleClick(row)} className='blue-btn'>Responder</button></td>
             </tr>
           ))}
         </tbody>
