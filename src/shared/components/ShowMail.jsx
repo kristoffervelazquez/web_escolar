e324c3455acbc1356a65c15542ec18b4cbad4586
Kristoffer Velazquez <kristoffervelazquez49@live.com>
2023-11-15 02:14:44 -0700

    css fixes

--- src/shared/components/ShowMail.jsx
+++ src/shared/components/ShowMail.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import './css/mailscreen.css';
 
-function ShowMail (){    
-    return(
-        
+function ShowMail() {
+    return (
+
         <form action="" className="mail-container">
 
-        <input type="text" className="input-info" placeholder="Para: "/>
-        <input type="text" className="input-info" placeholder="Asunto "/>       
-        
+            <input type="text" className="input-info" placeholder="Para: " />
+            <input type="text" className="input-info" placeholder="Asunto " />
+
             <section className="text-icons">
                 <i className="fi fi-br-bold"></i>
                 <i className="fi fi-br-italic"></i>
                 <i className="fi fi-br-underline"></i>
                 <i className="fi fi-br-interlining"></i>
                 <i className="fi fi-br-picture"></i>
-                <i className="fi fi-br-link-alt"></i>    
+                <i className="fi fi-br-link-alt"></i>
                 <i className="fi fi-rr-paperclip-vertical"></i>
-            </section>          
-            <textarea name="message" id="" cols="30" rows="10" className="message"> </textarea>                             
+            </section>
+            <textarea name="message" id="" cols="30" rows="10" defaultValue='' className="message"></textarea>
 
-            <br/>        
-                     
-<table className="">
-    <tr>
-        <td> <button className="cancel">Cancelar</button> </td>      
-        <td> <button className="send">Enviar</button></td>  
-    </tr>    
-  </table>
+            <br />
 
-    </form>                        
+            <div className='btn-container'>
+                <button className="send">Enviar</button>
+                <button className="cancel">Cancelar</button>
+            </div>
+
+
+
+        </form>
 
     );
 }
