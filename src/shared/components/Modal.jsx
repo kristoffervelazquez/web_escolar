a4e1dbe02fa60a9ebd85121a976867106a60a247
pinobaby <angel.ismael684@gmail.com>
2023-11-16 19:52:26 -0700

    Add modal to table activities

---
+++ src/shared/components/Modal.jsx
@@ -0,0 +1,24 @@
+
+ import "./css/modal.css";
+
+const Modal = ({ show, onClose, children }) => {
+const modalTransition = show ? "modal active" : "modal";
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className={modalTransition}>
+        <button className="modal-close" onClick={onClose}>
+          X
+        </button>
+        <div className="modal-content">{children}
+        
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
