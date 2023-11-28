7d1b64ad7a44cb64f71795c44fc25a65b7de775d
pinobaby <angel.ismael684@gmail.com>
2023-11-27 22:33:19 -0700

    add teacher pic to subjects cards

--- src/shared/components/Cards.jsx
+++ src/shared/components/Cards.jsx
@@ -1,25 +1,27 @@
 import cards from "../../CSS/Cards.css";
-import subjectsJson from "../../assets/db/subjects.json"
+import subjectsJson from "../../assets/db/subjects.json";
 
-
-let localUserInfo = localStorage.getItem('user');
+let localUserInfo = localStorage.getItem("user");
 let storedUser = JSON.parse(localUserInfo);
 
 const Cards = () => {
-
   return (
     <div className="container">
-
-      {subjectsJson.map((item, index) => (
-        storedUser.type === "Teacher" && storedUser.subjects === item.id ? (
-          storedUser.expedient === item.teacher ? (
-            <div className="grid-container">
+      {subjectsJson.map((item, index) =>
+        (storedUser.type === "Teacher" && 
+        storedUser.subjects === item.id && 
+        (storedUser.expedient === item.teacher) ||
+        storedUser.type === "Student" ? (
+            <div className="grid-container" key={index}>
               <div className="card">
                 <div className="top-section">
                   <div className="border"></div>
                   <div className="icons">
                     <div className="logo">
-
+                    <img
+                      src={item.teacherImg}
+                      alt={`${item.subjectName} teacher`}
+                    />
                     </div>
                   </div>
                 </div>
@@ -43,37 +45,10 @@ const Cards = () => {
               </div>
             </div>
           ) : null
-        ) : storedUser.type === "Student" ? (
-          <div className="card">
-            <div className="top-section">
-              <div className="border"></div>
-              <div className="icons">
-                <div className="logo">
-                </div>
-              </div>
-            </div>
-            <div className="bottom-section">
-              <span className="title">{item.subjectName}</span>
-              <div className="row row1">
-                <div className="item">
-                  <span className="big-text">23</span>
-                  <span className="regular-text">Calificadas</span>
-                </div>
-                <div className="item">
-                  <span className="big-text">4</span>
-                  <span className="">Tareas por hacer</span>
-                </div>
-                <div className="item">
-                  <span className="big-text">1</span>
-                  <span className="regular-text">Vencidas</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        ) : null))}
+        ) 
+      )}
     </div>
   );
-
 };
 
 export default Cards;
