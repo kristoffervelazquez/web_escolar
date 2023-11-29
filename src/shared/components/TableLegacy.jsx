531c49658338c9dddd41a86129dd47db506f0e16
pinobaby <angel.ismael684@gmail.com>
2023-11-28 23:10:47 -0700

    Estilizar el tableLegacy

--- src/shared/components/TableLegacy.jsx
+++ src/shared/components/TableLegacy.jsx
@@ -2,12 +2,16 @@ import "./css/table.css";
 import Assignment from "../../classes/assignment";
 import { useState } from "react";
 import Modal from "./Modal";
+import "./CSS/modal.css";
+import "./css/tableLegacy.css"
 
 const user = JSON.parse(localStorage.getItem("user"));
 
 function Table({ rows, columns }) {
   const [showModal, setShowModal] = useState(false);
   const [assignment, setAssignment] = useState(null);
+  const [fileDragged, setFileDragged] = useState(false);
+  const [file, setFile] = useState(null);
 
   const handleClick = async (row) => {
     const task = await Assignment.getAssignmentById(row.ID);
@@ -18,6 +22,28 @@ function Table({ rows, columns }) {
 
   const closeModal = () => {
     setShowModal(false);
+  };
+  
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    setFileDragged(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setFileDragged(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const selectedFile = e.dataTransfer.files[0];
+    setFile(selectedFile);
+    setFileDragged(false);
   };
 
   let localUserInfo = localStorage.getItem("user");
@@ -63,35 +89,39 @@ function Table({ rows, columns }) {
       </table>
 
 
-      <Modal show={showModal} onClose={closeModal}>
-        
-       
-        { assignment && 
-        
-            user.type == "Student"?
-            <div>
-            {/* Aquí puedes mostrar la información del elemento seleccionado */}
-            <h1>{assignment.title}</h1>
-            <h2>{assignment.description}</h2>
-            <h2>Adjuntar Archivo</h2>
-            <input type="file" />
-            <br />
-            <h2>Comentarios para el profesor</h2>
-            <textarea cols="80" rows="10"></textarea>
-            <br />
-            <button onClick={closeModal}>Cancelar</button>
-            <button onClick={closeModal}>Subir</button>
-            {/* Otros detalles del elemento */}
+     
+     <Modal show={showModal} onClose={closeModal} className="modal">
+      {assignment && user.type === 'Student' ? (
+        <div className="modal-content">
+          <h1>{assignment.title}</h1>
+          <h2>{assignment.description}</h2>
+          <h2>Adjuntar Archivo</h2>
+          <div
+            className={fileDragged ? 'file-drop-area active' : 'file-drop-area'}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
+          >
+            <input
+              type="file"
+              onChange={handleFileChange}
+              className="file-input"
+            />
+            <p>Arrastra y suelta archivos aquí o haz clic para seleccionar</p>
           </div>
-          : <div>
-            <input type="text" placeholder="Calificacion" />
-
-          </div>
-
-        }
-        
-        
-      </Modal>
+          {file && <p>Archivo seleccionado: {file.name}</p>}
+          <h2>Comentarios para el profesor</h2>
+          <textarea  className="text-areaaa" cols="93" rows="10"></textarea>
+          <br />
+          <button onClick={closeModal} className="tableLegacy">Cancelar</button>
+          <button onClick={closeModal} className="tableLegacy">Subir</button>
+        </div>
+      ) : (
+        <div>
+          <input type="text" placeholder="Calificacion" />
+        </div>
+      )}
+    </Modal>
     </div>
   );
 }
